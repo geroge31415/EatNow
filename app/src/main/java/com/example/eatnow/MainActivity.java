0525286fc7b528ce73869b263d8be19d35e644c7
geroge31415 <kyohei.k31415@icloud.com>
2023-11-10 20:38:19 +0900

    First day

--- app/src/main/java/com/example/eatnow/MainActivity.java
+++ app/src/main/java/com/example/eatnow/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.eatnow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private final String[] spinnerItems = {"300", "500", "1000", "2000","3000"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Spinner spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                spinnerItems
+        );
+        spinner.setAdapter(adapter);
     }
 }
